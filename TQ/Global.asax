ddf42a66fb09d4661c10aa472ec7eb6bf85a2e7a
Tofomb <tobias.c.forsberg@gmail.com>
2019-01-10 11:07:48 +0100

    Add project files.

---
+++ TQ/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TQ.MvcApplication" Language="C#" %>
---
+++ TQ/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TQ.Startup))]
+namespace TQ
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
